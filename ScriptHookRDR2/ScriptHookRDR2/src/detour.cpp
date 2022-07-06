eae1e54ae10f776539cdec4b6a4e0532f03ceae0
kepmehz <60520698+kepmehz@users.noreply.github.com>
2022-07-06 17:47:28 +0200

    yeah, I was absent for too long

---
+++ ScriptHookRDR2/ScriptHookRDR2/src/detour.cpp
@@ -0,0 +1,19 @@
+#include "../pch.h"
+
+void Detour::queue(const char* name, LPVOID target, LPVOID ownFunc, LPVOID* original)
+{
+	m_hooks.push(hooks(name, target, ownFunc, original));
+	globalvars::g_LogInstance->Write("Queued {} for hooking", name);
+}
+
+void Detour::hook()
+{
+	while (!m_hooks.empty())
+	{
+		auto hook = m_hooks.top();
+		// Do Minhook stuff
+		// if mh_ok print:
+		globalvars::g_LogInstance->Write("Hooked {}", hook.name);
+		m_hooks.pop();
+	}
+}
--- ScriptHookRDR2/ScriptHookRDR2/src/detour.hpp
+++ ScriptHookRDR2/ScriptHookRDR2/src/detour.hpp
@@ -12,23 +12,9 @@ class Detour
 {
 public:
 
-	void queue(const char* name, LPVOID target, LPVOID ownFunc, LPVOID* original)
-	{
-		m_hooks.push(hooks(name, target, ownFunc, original));
-		globalvars::g_LogInstance->Write("Queued {} for hooking", name);
-	}
+	void queue(const char* name, LPVOID target, LPVOID ownFunc, LPVOID* original);
 
-	void hook()
-	{
-		while (!m_hooks.empty())
-		{
-			auto hook = m_hooks.top();
-			// Do Minhook stuff
-			// if mh_ok print:
-			globalvars::g_LogInstance->Write("Hooked {}", hook.name);
-			m_hooks.pop();
-		}
-	}
+	void hook();
 
 private:
 	void* m_OriginalRageScrThreadUpdate{};
